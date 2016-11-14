0d660c5bb03be989323ef7e4b8227900439eabf1
Cameron Dutro <camertron@gmail.com>
2016-11-14 11:11:55 -0800

    Use IO#each_line instead of read and split

--- lib/abroad/extractors/txt/lines_extractor.rb
+++ lib/abroad/extractors/txt/lines_extractor.rb
@@ -10,9 +10,9 @@ module Abroad
         def each_entry
           return to_enum(__method__) unless block_given?
 
-          stream.read.split(/\r?\n/).each_with_index do |line, idx|
+          stream.each_line.with_index do |line, idx|
             # keys should always be strings
-            yield idx.to_s, line
+            yield idx.to_s, line.chomp
           end
         end
       end
